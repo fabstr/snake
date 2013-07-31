e1a6e2a40c55d33a13c51bc63f2ff1b97e5ccdf8
Fabian Ström <fs@fabianstrom.se>
2013-07-31 16:28:03 +0200

    log functionallity.

---
+++ mlog.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <time.h>
+
+#ifndef MLOG_H
+#define MLOG_H
+
+#define LOGFILE "snake.log"
+
+#define mlog(...) {\
+	char *logline = NULL; \
+	asprintf(&logline, __VA_ARGS__); \
+	write_log(logline); \
+	free(logline); }
+
+void write_log(char *msg);
+
+#endif
